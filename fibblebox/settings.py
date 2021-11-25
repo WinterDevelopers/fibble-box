96d5fab3548b3f04d0dd1ef5a0ef663c0112efd0
WinterDevelopers <winterdevelopers@gmail.com>
2021-11-25 06:32:20 +0100

    settings

--- fibblebox/settings.py
+++ fibblebox/settings.py
@@ -27,9 +27,9 @@ MEDIA_DIR = BASE_DIR/ 'media'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'[]
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
