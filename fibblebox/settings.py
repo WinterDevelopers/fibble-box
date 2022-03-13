2d1524824d59d006f85fb456e95a4f803842ac98
WinterDevelopers <winterdevelopers@gmail.com>
2022-03-13 22:34:55 +0100

    ..

--- fibblebox/settings.py
+++ fibblebox/settings.py
@@ -29,7 +29,7 @@ STATIC_DIR = BASE_DIR/ 'static'
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["178.62.97.24","fibblebox.com","127.0.0.1"]
 if not DEBUG:
@@ -154,6 +154,6 @@ SECRET_KEY = 'sk_test_9b966cca4e253f6c3cf4394df21957b30990d882'
 PUBLIC_KEY = 'pk_test_d62d0abc9b452417dcded834b109dfe257cd425a'
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = ''
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
