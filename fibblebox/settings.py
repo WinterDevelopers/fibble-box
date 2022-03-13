599261fe8c880e0796db3142a3bc6bc2ba7819fe
WinterDevelopers <winterdevelopers@gmail.com>
2022-03-13 19:40:26 +0100

    ...

--- fibblebox/settings.py
+++ fibblebox/settings.py
@@ -101,7 +101,7 @@ else:
             'PORT': '',
             }   
         }
-    #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -152,4 +152,4 @@ SECRET_KEY = 'sk_test_9b966cca4e253f6c3cf4394df21957b30990d882'
 PUBLIC_KEY = 'pk_test_d62d0abc9b452417dcded834b109dfe257cd425a'
 
 
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
